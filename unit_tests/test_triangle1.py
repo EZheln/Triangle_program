c93dd9cc5f2f698ae9564d4dccb7eef9a320cc1c
Olena Zhelnys'ka <ezhelnitskaya@gmail.com>
2018-05-12 15:43:28 +0300

    test cases were added

--- unit_tests/test_triangle1.py
+++ unit_tests/test_triangle1.py
@@ -10,4 +10,12 @@ def test_def_small_test_2():
     second_value = triangle1.def_small_test(12,12,5)
     assert second_value == 2
 
+def test_def_small_test_3():
+    third_value = triangle1.def_small_test(8,25,18)
+    assert third_value == 1
+
+def test_def_small_test_4():
+    fourth_value = triangle1.def_small_test(25,25,25)
+    assert fourth_value == 3
+
 
